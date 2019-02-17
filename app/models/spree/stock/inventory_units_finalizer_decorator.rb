f572127c0f2d7fb76a092ed80ad1e1ba9e2792fd
Sean <seand7565@gmail.com>
2019-02-17 08:49:49 -0600

    Pulling in unstock fix from Solidus 2.8.1

--- app/models/spree/stock/inventory_units_finalizer_decorator.rb
+++ app/models/spree/stock/inventory_units_finalizer_decorator.rb
@@ -5,7 +5,7 @@ Spree::Stock::InventoryUnitsFinalizer.class_eval do
       inventory_units_for_shipment.group_by(&:line_item_id).each_value do |units|
         shipment = units.first.shipment
         line_item = units.first.line_item
-        shipment.stock_location.unstock line_item.variant, inventory_units.sum(&:quantity), shipment
+        shipment.stock_location.unstock line_item.variant, units.sum(&:quantity), shipment
       end
     end
   end
--- spec/models/spree/stock/inventory_units_finalizer_spec.rb
+++ spec/models/spree/stock/inventory_units_finalizer_spec.rb
@@ -10,4 +10,26 @@ describe "Spree::Stock::InventoryUnitsFinalizer" do
       expect { finalizer.run! }.to change { variant.stock_items.first.count_on_hand }.by(-3)
     end
   end
+
+  context "when finalizing an order with multiple line_items" do
+    let(:order)          { create(:order_with_line_items, line_items_count: 2) }
+    let(:inventory_unit) { create(:inventory_unit, order: order, variant: order.line_items.first.variant, shipment: order.shipments.first) }
+    let(:inventory_unit_2) { create(:inventory_unit, order: order, variant: order.line_items.second.variant, shipment: order.shipments.first) }
+    let(:stock_item) { inventory_unit.variant.stock_items.first }
+    let(:stock_item_2) { inventory_unit.variant.stock_items.first }
+
+    before do
+      stock_item.set_count_on_hand(10)
+      stock_item_2.set_count_on_hand(10)
+      inventory_unit.update_attributes!(pending: true)
+      inventory_unit_2.update_attributes!(pending: true)
+    end
+
+    subject { Spree::Stock::InventoryUnitsFinalizer.new([inventory_unit, inventory_unit_2]).run! }
+
+    it "unstocks the variant with the correct quantity" do
+      expect { subject }.to change { stock_item.reload.count_on_hand }.from(10).to(9)
+      .and change { stock_item_2.reload.count_on_hand }.from(10).to(9)
+    end
+  end
 end
